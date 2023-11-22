d8cbaca65a7cc72994c6bb927060aa362b412e5f
Thomas Lerr <leyotsapon@central.co.th>
2023-11-22 10:33:23 +0700

    feat: create pages add new fonts, nav

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+});
+
+export const lusitana = Lusitana({
+   subsets: ['latin'],
+    weight: ['400', '700'],
+});
