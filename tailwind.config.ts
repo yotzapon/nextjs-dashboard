8c873662a85a9a350f1f3b3994168ff5ffcd6392
Thomas Lerr <leyotsapon@central.co.th>
2023-11-25 10:56:36 +0700

    chore: add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+      require('@tailwindcss/forms'),
+      require("daisyui"),
+  ],
 };
 export default config;
