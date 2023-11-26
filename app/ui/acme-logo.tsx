1873b0634c4aacd6cf4fd9af15766d92a43096bf
Thomas Lerr <leyotsapon@central.co.th>
2023-11-26 09:45:35 +0700

    chore: update store name and icon

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import {BuildingOffice2Icon, GlobeAltIcon} from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
@@ -6,8 +6,9 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/*<GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />*/}
+      <BuildingOffice2Icon className="h-12 w-12" />
+      <p className="text-[44px]">HH</p>
     </div>
   );
 }
